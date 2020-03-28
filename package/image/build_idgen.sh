90c367aac326997b833bb96b617cb3a7987ebc68
mgoblin <mike.golovanov@gmail.com>
2020-03-28 21:14:36 +0300

    fix build_idgen

--- package/image/build_idgen.sh
+++ package/image/build_idgen.sh
@@ -12,20 +12,16 @@ VERSION=1.0
 
 container=$(buildah from scratch)
 echo "Create container $container"
-#mnt=$(buildah mount $container)
-#echo "Mount container filesystem to $mnt"
 buildah add $container bin/users_idgen /idgen
 
 buildah config --cmd "/idgen" $container
 buildah config --port 8080 $container 
 
 img=$(buildah commit $container)
-#buildah umount $container
 
-
-buildah tag $img $NAME:$VERSION
+buildah tag $NAME:$VERSION
 echo "Create image $img done"
  
 buildah login -u mikegolovanov docker.io
-buildah push $img 
+buildah push $img $NAME:$VERSION 
 buildah logout docker.io
