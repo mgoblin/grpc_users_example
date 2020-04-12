956394bcffd9cf85bb804b00b960e167d99eaef4
mgoblin <mike.golovanov@gmail.com>
2020-04-12 10:03:46 +0300

    user .env for web client

--- cmd/web_client/main.go
+++ cmd/web_client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"mgoblin/users_grpc/internal/web"
 	"net/http"
@@ -8,23 +9,37 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 
 	v1 "mgoblin/users_grpc/internal/service/v1/client"
+	config "mgoblin/users_grpc/internal/users/client/config"
 )
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	log.Println("Users app web client starting")
 	r := mux.NewRouter()
 	h := handlers.LoggingHandler(os.Stdout, r)
 
-	address := "localhost:7777"
+	cfg := config.New()
+	usersAddress := cfg.UserService.URL
+	port := cfg.WebConfig.TCPPort
 
-	c := v1.NewUsersClient(&address)
+	c := v1.NewUsersClient(&usersAddress)
 	defer c.Close()
+
+	log.Printf("Listening on %s", fmt.Sprintf(":%d", port))
+	log.Printf("Users service address is %s", usersAddress)
 
 	r.HandleFunc("/", web.IndexHandler(*c))
 
-	if err := http.ListenAndServe(":8081", h); err != nil && err != http.ErrServerClosed {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), h); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("listen: %s\n", err)
 	}
 }
